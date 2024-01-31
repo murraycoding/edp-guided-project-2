33606270d3e92a487b6656efed569a1599999ca1
Al <Al@git.com>
2024-01-31 11:09:36 -0500

    api get/planets added

--- swapi_backend/server.js
+++ swapi_backend/server.js
@@ -3,15 +3,45 @@ var express = require('express');
 var {MongoClient} = require('mongodb');
 
 var app = express();
+app.use(express.json());
 
 const port = 5000;
-const db = client.db(swapi);
+
+const url = "mongodb://127.0.01:27017"
 
 
 app.listen(port);
 
+//*
 
-app.get("/api/planets" , (req,res) =>{
+//*/
 
+
+async function getAllPlanets(){
+    try{
+        const collection = db.collection('planet');
+        const planets = await collection.find().toArray();
+       
+    } catch (e){
+        console.error(e);
+    }
 }
-)
+
+
+app.get("/api/planets" , async  (req,res) =>{
+    //retrieve planets from db
+    try{
+        console.log("hello");
+        const client = await MongoClient.connect(url);
+        console.log("1");
+        const db = client.db('swapi');
+        console.log("2");
+        const collection = db.collection('planets');
+        console.log("3");
+        const planets = await collection.find().toArray();
+        client.close();
+    }catch (error){
+        console.error(error);
+    }
+    
+});
