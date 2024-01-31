41aa14d0b0e1213c40938255de41bab4a121c4b7
Al <Al@git.com>
2024-01-31 11:32:17 -0500

    added character and film routes

--- swapi_backend/server.js
+++ swapi_backend/server.js
@@ -30,6 +30,7 @@ async function getAllPlanets(){
 
 app.get("/api/planets" , async  (req,res) =>{
     //retrieve planets from db
+    // curl http://localhost:5000/api/planets
     try{
         console.log("hello");
         const client = await MongoClient.connect(url);
@@ -44,5 +45,72 @@ app.get("/api/planets" , async  (req,res) =>{
     }catch (error){
         console.error(error);
     }
-    
+});
+
+app.get("/api/planets" , async  (req,res) =>{
+    //retrieve planets from db
+    // curl http://localhost:5000/api/planets
+    try{
+        const client = await MongoClient.connect(url);
+        const db = client.db('swapi');
+        const collection = db.collection('planets');
+        const planets = await collection.find().toArray();
+        client.close();
+        res.json(planets);
+    }catch (error){
+        console.error(error);
+    }
+});
+
+app.get("/api/characters" , async  (req,res) =>{
+    //retrieve planets from db
+    // curl http://localhost:5000/api/planets
+    try{
+        const client = await MongoClient.connect(url);
+        const db = client.db('swapi');
+        const collection = db.collection('characters');
+        const characters = await collection.find().toArray();
+        client.close();
+        res.json(characters);
+    }catch (error){
+        console.error(error);
+    }
+});
+
+app.get("/api/films" , async  (req,res) =>{
+    //retrieve planets from db
+    // curl http://localhost:5000/api/films
+    try{
+        const client = await MongoClient.connect(url);
+        const db = client.db('swapi');
+        const collection = db.collection('films');
+        const films = await collection.find().toArray();
+        client.close();
+        res.json(films);
+    }catch (error){
+        console.error(error);
+    }
+});
+
+app.get("/api/characters/:id" , async  (req,res) =>{
+    //retrieve planets from db
+    // curl http://localhost:5000/api/films
+    console.log("in character id")
+    const id = req.params.id;
+    try{
+        const client = await MongoClient.connect(url);
+        const db = client.db('swapi');
+        const collection = db.collection('characters');
+        const characters = await collection.findOne({'id': id});
+        console.log(characters);
+        client.close();
+        if(characters){
+            res.json(characters);
+        } else {
+            res.status(404);
+        }
+        
+    }catch (error){
+        console.error(error);
+    }
 });
