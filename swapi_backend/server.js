91bdc5d916f305805a6b0e887bf17291e394fcdc
Al <Al@git.com>
2024-01-31 10:12:37 -0500

    backend stuff

---
+++ swapi_backend/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var {MongoClient} = require('mongodb');
+
+var app = express();
+
+const port = 5000;
+const db = client.db(swapi);
+
+
+app.listen(port);
+
+
+app.get("/api/planets" , (req,res) =>{
+
+}
+)
